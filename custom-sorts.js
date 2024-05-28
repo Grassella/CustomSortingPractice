622a046673d24186328ca9f9ad17973188097c7c
Ruben Cuello <kuelmusic@gmail.com>
2024-05-27 21:25:56 -0400

    ajl

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,23 +1,5 @@
 function ageSort(users) {
 
-  // for (let i = 0; i < users.length - 1; i++) {
-  //   let swap = false
-  //   for (let j = 0; j < users.length - i - 1; j++) {
-
-  //     if (users[j].age > users[j + 1].age) {
-  //       let temp = users[j].age
-  //       console.log("temp: " + temp)
-  //       users[j].age = users[j + 1].age
-  //       users[j + 1].age = temp
-
-  //       swap = true
-  //     }
-  //   }
-  //   if (!swap) {
-  //     break
-  //   }
-  // }
-
   for (let i = 0; i < users.length - 1; i++) {
     for (let j = 0; j < users.length - i - 1; j++) {
       if (users[j].age > users[j + 1].age) {
@@ -29,6 +11,7 @@ function ageSort(users) {
     }
   }
   return users;
+
 
 }
 
@@ -42,93 +25,129 @@ function ageSort(users) {
 
 function oddEvenSort(arr) {
 
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j]
-        arr[j] = arr[j + 1]
-        arr[j + 1] = temp
-      }
-    }
-  }
+  // for (let i = 0; i < arr.length - 1; i++) {
+  //   for (let j = 0; j < arr.length - i - 1; j++) {
+  //     if (arr[j] > arr[j + 1]) {
+  //       let temp = arr[j]
+  //       arr[j] = arr[j + 1]
+  //       arr[j + 1] = temp
+  //     }
+  //   }
+  // }
 
+  // let odd = []
+  // let even = []
+
+  // for (let i = 0; i < arr.length; i++) {
+  //   let num = arr[i]
+  //   if (num % 2 === 0) {
+  //     even.push(num)
+  //   }
+  //   else {
+  //     odd.push(num)
+  //   }
+
+
+  // }
   let odd = []
   let even = []
-
-  for (let i = 0; i < arr.length; i++) {
-    let num = arr[i]
-    if (num % 2 === 0) {
-      even.push(num)
+  for (const ele of arr) {
+    if(ele % 2 === 0){
+      even.push(ele)
+    }else{
+      odd.push(ele)
     }
-    else {
-      odd.push(num)
-    }
-
-
   }
-
+    odd.sort((a,b) => a - b)
+    even.sort((a,b) => a - b)
   return odd.concat(even)
 }
 
 function validAnagrams(s, t) {
+    let word1 = s.split('').sort().join()
+    let word2 = t.split('').sort().join()
+   return  word1 === word2
 
-  let obj = {}
-  if (s.length === t.length) {
 
-    for (let i = 0; i < s.length; i++) {
-      let letter = s[i]
-      let letter2 = t[i]
-      if (obj[letter] === undefined) {
-        obj[letter] = 1
+//   let obj = {}
+//   if (s.length === t.length) {
 
-      }
-      else {
-        obj[letter]++
-      }
-      if (obj[letter2] === undefined) {
-        obj[letter2] = 1
+//     for (let i = 0; i < s.length; i++) {
+//       let letter = s[i]
+//       let letter2 = t[i]
+//       if (obj[letter] === undefined) {
+//         obj[letter] = 1
 
-      }
-      else {
-        obj[letter2]++
-      }
+//       }
+//       else {
+//         obj[letter]++
+//       }
+//       if (obj[letter2] === undefined) {
+//         obj[letter2] = 1
 
-    }
+//       }
+//       else {
+//         obj[letter2]++
+//       }
 
-  }
-  else {
-    return false
-  }
-  for(let el in obj) {
-    if(obj[el] % 2 !== 0) {
-      return false
-    }
-  }
-  return true
-  
+//     }
+
+//   }
+//   else {
+//     return false
+//   }
+//   for(let el in obj) {
+//     if(obj[el] % 2 !== 0) {
+//       return false
+//     }
+//   }
+//   return true
+
 }
 
 function reverseBaseSort(arr) {
-  // Your code here 
+  return arr.sort((a, b) => {
+    let baseA = Math.floor(Math.log10(a));
+    let baseB = Math.floor(Math.log10(b));
+    if(baseA === baseB) return a - b;
+    return b - a;
+  })
 }
+
 
 function frequencySort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j]
-        arr[j] = arr[j + 1]
-        arr[j + 1] = temp
-      }
+  let freqMap = {}
+
+  // grab a tally of numbers
+  arr.forEach(num => {
+    if (freqMap[num]) {
+      freqMap[num]++
+    } else {
+      freqMap[num] = 1
     }
-  } 
-  return arr.reverse()
+  })
+
+  // sort based on number next to value
+  // if the next number is the number
+  // put it together
+  // otherwise sort in descending order
+  return arr.sort((a, b) => {
+    if (freqMap[a] !== freqMap[b]) {
+      return freqMap[a] - freqMap[b]
+    }
+
+    return b - a
+  })
 }
 
 
-let s = "anagram";
-let t = "nagaram";
-console.log(validAnagrams(s, t))
+const arr1 = [11, 1, 101, 0, 10, 100];
+const arr2 = [1, 45, 164, 6, 31, 90, 671];
+
+console.log(oddEvenSort(arr1));      // => [100, 101, 10, 11, 0, 1]
+// console.log(reverseBaseSort(arr2));      // => [164, 671, 31, 45, 90, 1, 6]
+
+
 
 module.exports = [
   oddEvenSort,
